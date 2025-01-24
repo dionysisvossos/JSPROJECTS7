bff22a1875f5e20a7f276da517b0abf592083c61
Dionysis.Vossos <dionysis.vossos@windowslive.com>
2025-01-24 18:57:24 +0200

    updates

--- review/7a-arrow-celcius.js
+++ review/7a-arrow-celcius.js
@@ -1,1 +1,3 @@
-const celsiusToFahrenheit = (celsius) => {return (celsius * 9 / 5) + 32}
+const celsiusToFahrenheit = (celsius) => { return (celsius * 9 / 5) + 32 }
+
+console.log(celsiusToFahrenheit(0))
