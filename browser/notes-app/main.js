34728c1abfa942a2303d45d39b481fe252856d4c
Dionysis.Vossos <dionysis.vossos@windowslive.com>
2025-02-23 14:26:27 +0200

    feat: notes app created

--- browser/notes-app/main.js
+++ browser/notes-app/main.js
@@ -17,6 +17,76 @@ window.addEventListener('DOMContentLoaded', function() {
             onInsertHandler({key: count + 1, note: e.target.value.trim(), softDeleted: false})
         }
     })
-
-
 })
+
+function printGRDate() {
+    const currentDate = new Date()
+    const day = currentDate.getDay()
+    const date = currentDate.getDate()
+    const month = currentDate.getMonth()
+    const year = currentDate.getFullYear()
+    const hours = currentDate.getHours()
+    const minutes = currentDate.getMinutes()
+    const seconds = currentDate.getSeconds()
+
+    const dayStr = daysGR[day]
+    const monthStr = monthsGR[month]
+
+    const dateStr = `${dayStr}, ${date} ${monthStr} ${year}`
+    const timeStr = `${(hours < 10) ? '0' : ''}${hours}:${(minutes < 10) ? '0' : ''}${minutes}:${(seconds < 10) ? '0' : ''}${seconds}`
+
+    document.querySelector('#dateTxt').innerHTML = dateStr + '<br>' + timeStr
+}
+/**
+ * Controller in MVC
+ * @param {Object} data 
+ * @returns nothing if data.none is empty or null
+ */
+function onInsertHandler(data) {
+    if(!data.note) return
+    insertNote(data)
+    reset()
+}
+/**
+ * Model function, inserts a new note
+ * @param {Object} note 
+ */
+function insertNote(note) {
+    notes = [...notes, note]
+    count++
+    renderNotes()
+}
+/**
+ * View rerendering function
+ */
+function renderNotes() {
+    const container = document.getElementById('notesWrapper')
+    
+    container.innerHTML = notes.map(note => `<div id="${'noteTemplate' + note.key}" class="flex justify-between items-center px-[10px] py-[2px] border-bottom border-black">
+        <div id="${'noteInfo' + note.key}" class="flex items-center">
+            <input id="${'noteCheck' + note.key}" type="checkbox" class="w-[25px] h-[25px] mr-[5px]" ${note.softDeleted ? 'checked' : ''} onclick="strikeThrough(${note.key})">
+            <label id="${'noteTxt' + note.key}" for="${'noteCheck' + note.key}" class="w-[200px] max-h-[150px] leading-[1.2rem] overflow-hidden break-words whitespace-normal text-base ${note.softDeleted ? 'line-through text-gray-500' : ''}">${note.note}</label>
+        </div>
+        <button type="button" id="${'noteDelBtn' + note.key}" class="w-[35px] h-[35px]" onclick="deleteNote(${note.key})">X</button>
+    </div>`).join("")
+}
+
+/**
+ * Model functions
+ */
+
+function strikeThrough(key) {
+    notes = notes.map(note => note.key === key ? {...note, softDeleted: !note.softDeleted} : {...note})
+    renderNotes()
+}
+
+function deleteNote(key) {
+    notes = notes.filter(note => note.key !== key)
+    renderNotes()
+}
+/**
+ * View functions
+ */
+function reset() {
+    document.querySelector('#inputNote').value = ""
+}
